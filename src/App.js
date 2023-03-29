660c54b9a9a84329388251e1496218e20df05ee6
LegendH <774659715@qq.com>
2023-03-29 16:24:20 +0800

    add antd & sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login";
+import Layout from "./pages/Layout";
+import {Button} from "antd";
 
 function App() {
     return (
-        <div className="App">
-            app
-        </div>
+        // 路由配置
+        <BrowserRouter>
+            <div className="App">
+                <Button type='primary'>Button</Button>
+                <Routes>
+                    {/*创建路由path和组件的对应关系*/}
+                    <Route path='/' element={<Layout/>}></Route>
+                    <Route path='/login' element={<Login/>}></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
