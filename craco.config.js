440831cc15cfa12737ef7357dc46f492972ae393
LegendH <774659715@qq.com>
2023-04-03 16:55:52 +0800

    add webpack config(craco) & path @

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+// 添加自定义对于webpack的配置
+const path = require('path')
+module.exports = {
+    // webpack配置
+    webpack: {
+        alias: {
+            // 约定，使用@表示src文件所在路径
+            '@': path.resolve(__dirname, 'src')
+        }
+    }
+}
